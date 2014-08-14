afe4c8445ebb13970c0fd1914130e7503a39e6f0
James Van Mil <james.vanmil@gmail.com>
2014-08-13 22:33:20 -0400

    Workaround for testing across has_many through associations

--- spec/factories/bib_view.rb
+++ spec/factories/bib_view.rb
@@ -1,8 +1,14 @@
 FactoryGirl.define do
-  factory :bib_view_1, class: BibView do
+  factory :item_view, class: ItemView do
+    id 1
+    location_code "ula"
+  end
+
+  factory :bib_view, class: BibView do
     id 1
     record_num 1000001
     title "Title 1"
+
     association :record_metadata, factory: :record_metadata_1
   end
 end
--- spec/models/active_sierras_spec.rb
+++ spec/models/active_sierras_spec.rb
@@ -1,19 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe "ActiveSierra", :type => :request do
-  describe "ActiveSierra BibView" do
-    bib = FactoryGirl.build_stubbed(:bib_view_1)
+  describe "BibView" do
+    before(:each) do
+      @bib = FactoryGirl.build_stubbed :bib_view
+      @items = FactoryGirl.build_stubbed_list(:item_view, 1)
+      @bib.stub(:item_views).and_return(@items)
+    end
 
     it "has a title" do
-      expect(bib.title).to eq("Title 1")
+      expect(@bib.title).to eq("Title 1")
     end
 
     describe "RecordMetadata" do
-
       it "has a created date" do
-        expect(bib.record_metadata.creation_date_gmt.class).to eq(ActiveSupport::TimeWithZone)
+        expect(@bib.record_metadata.creation_date_gmt.class).to eq(ActiveSupport::TimeWithZone)
       end
+    end
 
+    describe "has ItemView" do
+      it "with a location" do
+        expect(@bib.item_views.first.location_code).to eq("ula")
+      end
     end
   end
 end
